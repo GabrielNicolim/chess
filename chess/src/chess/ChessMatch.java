3ef32a5c129982f197bdb4824d8a06f05d8d3905
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-21 14:22:48 -0300

    Movimentos de Torre (Rook)

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -33,6 +33,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 		
 		validateSourcePosition(source);
+		validateTargetPosition(source, target); 
 		
 		Piece capturedPiece = makeMove(source, target);
 		
@@ -53,6 +54,12 @@ public class ChessMatch {
 		
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possibles moves for the chosen piece");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 	
