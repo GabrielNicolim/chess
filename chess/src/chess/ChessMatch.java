32ecf7969c4be8c7597e98f6dfc6a4f22014224b
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-21 22:01:57 -0300

    Mudança de jogador e contador de turnos adicionado

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8); 
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup(); 
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer; 
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -42,6 +54,8 @@ public class ChessMatch {
 		
 		Piece capturedPiece = makeMove(source, target);
 		
+		nextTurn();
+		
 		return (ChessPiece) capturedPiece;
 	}
 	
@@ -57,6 +71,10 @@ public class ChessMatch {
 			throw new ChessException("There is no piece on source position"); 
 		}
 		
+		if(currentPlayer != ((ChessPiece) board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
+		
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possibles moves for the chosen piece");
 		}
@@ -66,6 +84,11 @@ public class ChessMatch {
 		if(!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
