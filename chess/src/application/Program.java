a7f323a508b528e368536ef6da1b439e21912cef
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-23 16:18:03 -0300

    Fix tratamento melhor para a promoção

--- chess/src/application/Program.java
+++ chess/src/application/Program.java
@@ -41,8 +41,14 @@ public class Program {
 				
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					chessMatch.replacePromotedPiece(type.toUpperCase());
+					String type = sc.nextLine().toUpperCase();
+					
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
