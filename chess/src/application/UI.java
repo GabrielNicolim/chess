726320417ed7db80ad8bcbcbd641216795420ab6
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-23 15:41:45 -0300

    En Passant

--- chess/src/application/UI.java
+++ chess/src/application/UI.java
@@ -87,13 +87,17 @@ public class UI {
 
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
+			System.out.print(ANSI_GREEN);
 			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_RESET);
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
+		System.out.print(ANSI_GREEN);
 		System.out.println("  a b c d e f g h");
+		System.out.print(ANSI_RESET);
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
